2e1485eebab237f497c184c987d207817021cd32
baominh5xx2 <baominh5xx2@gmail.com>
2025-02-07 13:20:34 +0700

    update

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException
 from app.database.mongodb import get_db
-
+from app.api.v1.auth.login.router import router as login_router
+from app.api.v1.auth.register.router import router as register_router
+from app.api.v1.flashcard.router import router as flashcard_router
 
 app = FastAPI()
 
@@ -13,3 +15,8 @@ async def test_db_connection(db=Depends(get_db)):
         return {"status": "success", "message": "Connected to MongoDB", "result": result}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to connect to MongoDB: {e}")
+
+# Include routers
+app.include_router(login_router, prefix="/api/v1/auth", tags=["auth"])
+app.include_router(register_router, prefix="/api/v1/auth", tags=["auth"])
+app.include_router(flashcard_router, prefix="/api/v1/flashcard", tags=["flashcard"])
